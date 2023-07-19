28e2b9d9af46ca2d16f46922abaac281100e290b
Mahzabin <mashrufa1997@yahoo.co.uk>
2023-07-19 20:15:48 +0100

    sorted styling for form and color buttons

--- client/src/components/userPreferences/ColorSelection.js
+++ client/src/components/userPreferences/ColorSelection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./ColorSelection.css"
 
 const ColorSelection = ({ handleColorClick }) => {
 
@@ -6,16 +7,16 @@ const ColorSelection = ({ handleColorClick }) => {
 
     return (
         <div className="Color-button-container">
-        <button id="Pink" onClick={(event) => handleColorClick(event)}>Pink</button>
-        <button id="Orange" onClick={(event) => handleColorClick(event)}>Orange</button>
-        <button id="Grey" onClick={(event) => handleColorClick(event)}>Grey</button>
-        <button id="Yellow" onClick={(event) => handleColorClick(event)}>Yellow</button>
-        <button id="Blue" onClick={(event) => handleColorClick(event)}>Blue</button>
-        <button id="Red" onClick={(event) => handleColorClick(event)}>Red</button>
-        <button id="Black" onClick={(event) => handleColorClick(event)}>Black</button>
-        <button id="Purple" onClick={(event) => handleColorClick(event)}>Purple</button>
-        <button id="Brown" onClick={(event) => handleColorClick(event)}>Brown</button>
-        <button id="White" onClick={(event) => handleColorClick(event)}>White</button>
+        <button className="pink-button" id="Pink" onClick={(event) => handleColorClick(event)}>Pink</button>
+        <button className="orange-button" id="Orange" onClick={(event) => handleColorClick(event)}>Orange</button>
+        <button className="grey-button" id="Grey" onClick={(event) => handleColorClick(event)}>Grey</button>
+        <button className="yellow-button" id="Yellow" onClick={(event) => handleColorClick(event)}>Yellow</button>
+        <button className="blue-button" id="Blue" onClick={(event) => handleColorClick(event)}>Blue</button>
+        <button className="red-button" id="Red" onClick={(event) => handleColorClick(event)}>Red</button>
+        <button className="black-button" id="Black" onClick={(event) => handleColorClick(event)}>Black</button>
+        <button className="purple-button" id="Purple" onClick={(event) => handleColorClick(event)}>Purple</button>
+        <button className="brown-button" id="Brown" onClick={(event) => handleColorClick(event)}>Brown</button>
+        <button className="white-button" id="White" onClick={(event) => handleColorClick(event)}>White</button>
       </div>
     )
 }
